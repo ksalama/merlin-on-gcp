dbbaf1cd292b1227650f01cfbfd490582074cf16
Khalid Salama <khalid.m.salama@gmail.com>
2021-07-13 16:55:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 . "/opt/conda/etc/profile.d/conda.sh"
 conda activate nvtabular_dev_11.0
 
-pip install -e NVTabular/.
 pip install tritonclient
 
 echo "Current conda environment:" $CONDA_DEFAULT_ENV
